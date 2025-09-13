02893adfabbfd65d6a059c13825122b3d0d83fea
gkelite-info <gkeliteinfo@gmail.com>
2025-09-13 15:13:34 +0530

    added header and sidenavbar

--- Layouts/Header.tsx
+++ Layouts/Header.tsx
@@ -1,7 +1,38 @@
-import React from "react"
+'use client';
 
-const Header = () => {
-  return <div className="w-full h-[70px] bg-amber-100">Header</div>
+import { Bell, MagnifyingGlass, Tray } from "phosphor-react";
+
+export default function Header() {
+  return (
+    <>
+      <div className="w-full h-[70px] bg-amber-400 flex items-center">
+        <div className="bg-[#9884D6] w-[108.5px] h-[100%] flex items-center justify-center border border-0 border-r-1">
+          <img src="/gkLogo.png" alt="gkLogo.png" className="w-[60%]" />
+        </div>
+        <div className="bg-[#9884D6] h-[100%] w-[100%] flex items-center justify-between pr-4">
+          <div className="flex items-center h-[100%] w-[60%] gap-5">
+            <div className="bg-red-400 h-[100%] w-[17%] flex items-center justify-center">
+              Logo
+            </div>
+            <div className="flex items-center bg-[#1C334E] gap-3 h-[60%] w-[70%] rounded-full px-3">
+              <MagnifyingGlass size={19} weight="bold" className="text-white cursor-pointer" />
+              <input
+                placeholder="Search employees or action (Ex: Leave Request)"
+                className="text-white focus:outline-none w-full"
+              />
+            </div>
+          </div>
+
+          {/* //icons */}
+          <div className="h-[100%] w-[12%] bg- flex items-center justify-center gap-4">
+            <Bell size={26} className="text-white cursor-pointer" />
+            <Tray size={26} className="text-white cursor-pointer" />
+            <div className="h-8 w-8 flex items-center justify-center bg-blue-400 rounded-full cusor-pointer">
+              <p className="cursor-pointer">X</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
-
-export default Header
