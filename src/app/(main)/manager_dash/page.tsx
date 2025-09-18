5917ca49376d73b3f4bcf8b5290e9a5d41cdcb2c
gkelite-info <gkeliteinfo@gmail.com>
2025-09-18 14:23:20 +0530

    left with table at the end

--- src/app/(main)/manager_dash/page.tsx
+++ src/app/(main)/manager_dash/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MapPin, Megaphone, Moon, PlusCircle, SignIn, SignOut, Sun, Trash } from "phosphor-react";
+import { CheckCircle, MapPin, Megaphone, Moon, SignIn, SignOut, Sun, WarningCircle, XCircle } from "phosphor-react";
 import { useEffect, useState } from "react";
 import NotificationCount from "../../../../utils/notificationCount";
 import TeamSnapshot from "../../../../features/manager/components/teamSnapshot";
@@ -8,7 +8,9 @@ import TeamLeads from "../../../../features/manager/components/teamLeads";
 import TaskCard from "../../../../utils/taskCard";
 import PendingTaskCard from "../../../../utils/pendingTaskcard";
 import LeaveTable from "../../../../utils/leaveTable";
-import PercentPie from "../../../../utils/pieChart";
+import PercentPie from "../../../../Utils/pieChart";
+import Table from "../../../../utils/Table/Table";
+import KeenSlider from "../../../../utils/keen-slider";
 
 
 export default function ManagerDashboard() {
@@ -92,13 +94,13 @@ export default function ManagerDashboard() {
                         </div>
                     </div>
                 </div>
-                <div className="bg-blue-400 h-auto flex justify-between">
-                    <div className="w-[50%] bg-red-400 p-2">
-                        <div className="bg-white rounded-lg flex items-center justify-between h-40 w-[100%] mt-1 shadow-md">
-                            <div className="w-[60%] h-[100%] bg-green-400 flex flex-col items-start justify-between p-3 rounded-l-lg">
+                <div className="bg-blue-00 h-auto flex justify-between">
+                    <div className="w-[50%] bg-red-00 p-4">
+                        <div className="bg-[#F9FAFB] rounded-lg flex items-center justify-between h-40 w-[100%] mt-1 shadow-md">
+                            <div className="w-[60%] h-[100%] bg-green-00 flex flex-col items-start justify-between p-3 rounded-l-lg">
                                 <div className="bg-red-00 h-[35%] w-[100%] flex justify-between">
                                     <p className="text-xs text-[#323232] font-medium">Today's Login Time</p>
-                                    <div className="h-[100%] w-[45%] bg-white flex flex-col justify-between">
+                                    <div className="h-[100%] w-[45%] flex flex-col justify-between">
                                         <div className="bg-blue-00 h-[49%] w-[100%] flex items-center gap-1">
                                             <p className="text-[#007752] font-medium text-xs">Log In</p>
                                             <SignIn size={19} color="#007752" weight="fill" />
@@ -158,15 +160,15 @@ export default function ManagerDashboard() {
                                 </div>
                             </div>
                         </div>
-                        <div className="bg-white h-82 mt-2 rounded-lg p-2">
+                        <div className="bg-[#F9FAFB] h-82 mt-2 rounded-lg p-2 shadow-md">
                             <TeamSnapshot />
                             <TeamLeads />
                         </div>
-                        <div className="h-92 bg-white flex flex-col rounded-lg mt-2">
+                        <div className="h-92 bg-[#F9FAFB] flex flex-col rounded-lg mt-2 p-3 shadow-md">
                             <TaskCard style="w-[100%]" gapStyle="w-[14%]" rounded="rounded-t-lg" boxShadow='shadow-none' customHeight="h-[60%]" />
                             <PendingTaskCard style="w-[100%]" rounded='rounded-b-lg' />
                         </div>
-                        <div className="bg-white h-143 mt-2 shadow-md rounded-lg p-3 flex flex-col items-center justify-center">
+                        <div className="bg-[#F9FAFB] h-143 mt-2 shadow-md rounded-lg p-3 flex flex-col items-center justify-center">
                             <div className="bg-red-00 w-[98%] flex flex-col items-center justify-center">
                                 <h3 className="text-[#111827] font-semibold">Attendance & Leave</h3>
                                 <div className="bg-[#F4F3FF] rounded-lg flex flex-col items-center h-82 w-[100%] shadow-md pt-3">
@@ -241,14 +243,181 @@ export default function ManagerDashboard() {
                                     </div>
                                 </div>
                                 <div className="h-40 w-[100%] bg-[#F4F3FF] mt-2 p-3 rounded-lg">
-                                    <p className="text-black">Vamshi</p>
+                                    Table
                                 </div>
                             </div>
 
                         </div>
                     </div>
-                    <div className="w-[50%] bg-gray-400 p-2">
-                        Right
+                    <div className="w-[50%] bg-gray-00 p-4 pt-5 pl-0">
+                        <div className="bg-[#F9FAFB] h-124 w-[100%] rounded-lg p-5 flex flex-col shadow-md">
+                            <div className="bg-indigo-00">
+                                <h3 className="text-md font-semibold text-[#111827]">Team Performance </h3>
+                                <h4 className="text-sm font-medium text-[#111827] ml-3 mt-2">Performance Highlights</h4>
+                            </div>
+                            <div className="bg-red-00 w-[100%] flex items-center mt-3">
+                                <CheckCircle size={28} color="#48A083" weight="fill" />
+                                <p className="text-[#464646] ml-1 text-xs">2 Employees exceeded targets last.....</p>
+                            </div>
+                            <div className="bg-green-00 w-[100%] flex items-center mt-2 pl-0.5">
+                                <div className="bg-[#DE483B] rounded-full h-6 w-6 flex items-center justify-center">
+                                    <WarningCircle size={18} color="#FFFFFF" />
+                                </div>
+                                <p className="text-[#464646] ml-1 text-xs ml-2">1 Employee lagging</p>
+                            </div>
+                            <div className="bg-gray-00 mt-5">
+                                <h2 className="text-md text-[#111827] font-semibold">Workload</h2>
+                                <div className="w-[100%] bg-indigo-00 flex flex-col mt-3">
+                                    <h3 className="text-sm text-[#464646] font-medium">John Doe</h3>
+                                    <div className="bg-[#47A082] w-[90%] h-8 rounded-md">
+                                    </div>
+                                </div>
+                                <div className="w-[100%] bg-indigo-00 flex flex-col mt-4">
+                                    <h3 className="text-sm text-[#464646] font-medium">Jane Smith</h3>
+                                    <div className="bg-[#47A082] w-[83%] h-8 rounded-md">
+                                    </div>
+                                </div>
+                                <div className="w-[100%] bg-indigo-00 flex flex-col mt-4 gap-1">
+                                    <h3 className="text-sm text-[#464646] font-medium">Bob Johnson</h3>
+                                    <div className="bg-[#DE483B] w-[20%] h-8 rounded-md">
+                                    </div>
+                                </div>
+                                <div className="w-[100%] bg-indigo-00 flex flex-col mt-4">
+                                    <h3 className="text-sm text-[#464646] font-medium">John Doe</h3>
+                                    <div className="bg-[#47A082] w-[90%] h-8 rounded-md">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-[#F9FAFB] h-92 p-0 pb-3 mt-2 rounded-lg shadow-md flex flex-col items-center">
+                            <div className="bg-red-00 w-[90%] h-[15%] rounded-lg mt-2">
+                                <h2 className="text-lg font-semibold text-[#111827]">Meetings Preview</h2>
+                                <p className="text-xs text-[#464646] mt-1">Synced with Outlook/ Google Calendar</p>
+                            </div>
+                            <div className="bg-indigo-00 h-[85%] w-[90%] flex flex-col justify-between">
+                                <div className="bg-gray-300 h-[22%] w-[80%] flex flex-col rounded-md p-2">
+                                    <h4 className="text-[#111111]">9:00 AM - 10:00 AM</h4>
+                                    <p className="text-[#6B6B6B] text-sm">Project Kick - Off</p>
+                                </div>
+                                <div className="bg-gray-300 h-[22%] w-[80%] flex flex-col rounded-md p-2">
+                                    <h4 className="text-[#111111]">1:00 PM - 2:00 PM</h4>
+                                    <p className="text-[#6B6B6B] text-sm">Team Weekly Sync</p>
+                                </div>
+                                <div className="bg-gray-300 h-[22%] w-[80%] flex flex-col rounded-md p-2">
+                                    <h4 className="text-[#111111]">3:00 PM - 4:00 PM</h4>
+                                    <p className="text-[#6B6B6B] text-sm">Client Call</p>
+                                </div>
+                                <div className="bg-gray-300 h-[22%] w-[80%] flex flex-col rounded-md p-2">
+                                    <h4 className="text-[#111111]">9:00 AM - 10:00 AM</h4>
+                                    <p className="text-[#6B6B6B] text-sm">Project Kick - Off</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-[#F9FAFB] h-143 mt-2 rounded-lg shadow-md flex flex-col items-start p-4">
+                            <h2 className="text-lg text-[#111827] font-semibold">Pending Approvals</h2>
+                            <div className="bg-indigo-00 h-[94%] w-[100%] mt-2 flex flex-col justify-between">
+                                <div className="bg-[#F9FAFB] h-[23%] rounded-lg border border-1 border-[#C3C3C3] flex">
+                                    <div className="bg-red-00 h-[100%] w-[55%] rounded-l-lg p-2 flex gap-4">
+                                        <div className="h-[100%] w-[25%] flex items-center justify-center">
+                                            <div className="h-17 w-17 bg-gray-400 flex items-center justify-center rounded-full">
+                                                I
+                                            </div>
+                                        </div>
+                                        <div className="bg-gray-00 h-[100%] w-[75%] flex flex-col justify-center">
+                                            <h3 className="font-semibold text-lg text-[#111827]">Jasmine</h3>
+                                            <h5 className="text-md text-[#575757] font-medium">Leave Request</h5>
+                                            <p className="text-xs text-[#575757] mt-1">Family Event</p>
+                                        </div>
+                                    </div>
+                                    <div className="h-[100%] w-[45%] rounded-r-lg p-2 flex items-center justify-center gap-2">
+                                        <button className="bg-[#3DB482] p-2 rounded-lg w-30 text-[#F9FAFB] font-semibold cursor-pointer">Approve</button>
+                                        <button className="bg-[#F66557] text-[#F9FAFB] font-semibold p-2 rounded-lg w-30 cursor-pointer">Reject</button>
+                                    </div>
+                                </div>
+                                <div className="bg-[#F9FAFB] h-[23%] rounded-lg border border-1 border-[#C3C3C3] flex">
+                                    <div className="bg-red-00 h-[100%] w-[55%] rounded-l-lg p-2 flex gap-4">
+                                        <div className="h-[100%] w-[25%] flex items-center justify-center">
+                                            <div className="h-17 w-17 bg-gray-400 flex items-center justify-center rounded-full">
+                                                I
+                                            </div>
+                                        </div>
+                                        <div className="bg-gray-00 h-[100%] w-[75%] flex flex-col justify-center">
+                                            <h3 className="font-semibold text-lg text-[#111827]">John</h3>
+                                            <h5 className="text-md text-[#575757] font-medium">Attendance Regulariz.....</h5>
+                                            <p className="text-xs text-[#575757] mt-1">Forget to Check-in</p>
+                                        </div>
+                                    </div>
+                                    <div className="h-[100%] w-[45%] rounded-r-lg p-2 flex items-center justify-center gap-2">
+                                        <button className="bg-[#3DB482] p-2 rounded-lg w-30 text-[#F9FAFB] font-semibold cursor-pointer">Approve</button>
+                                        <button className="bg-[#F66557] text-[#F9FAFB] font-semibold p-2 rounded-lg w-30 cursor-pointer">Reject</button>
+                                    </div>
+                                </div>
+                                <div className="bg-[#F9FAFB] h-[23%] rounded-lg border border-1 border-[#C3C3C3] flex">
+                                    <div className="bg-red-00 h-[100%] w-[55%] rounded-l-lg p-2 flex gap-4">
+                                        <div className="h-[100%] w-[25%] flex items-center justify-center">
+                                            <div className="h-17 w-17 bg-gray-400 flex items-center justify-center rounded-full">
+                                                I
+                                            </div>
+                                        </div>
+                                        <div className="bg-gray-00 h-[100%] w-[75%] flex flex-col justify-center">
+                                            <h3 className="font-semibold text-lg text-[#111827]">Emily</h3>
+                                            <h5 className="text-md text-[#575757] font-medium">Leave Request</h5>
+                                            <p className="text-xs text-[#575757] mt-1">Client Meeting</p>
+                                        </div>
+                                    </div>
+                                    <div className="h-[100%] w-[45%] rounded-r-lg p-2 flex items-center justify-center gap-2">
+                                        <button className="bg-[#3DB482] p-2 rounded-lg w-30 text-[#F9FAFB] font-semibold cursor-pointer">Approve</button>
+                                        <button className="bg-[#F66557] text-[#F9FAFB] font-semibold p-2 rounded-lg w-30 cursor-pointer">Reject</button>
+                                    </div>
+                                </div>
+                                <div className="bg-[#F9FAFB] h-[23%] rounded-lg border border-1 border-[#C3C3C3] flex">
+                                    <div className="bg-red-00 h-[100%] w-[55%] rounded-l-lg p-2 flex gap-4">
+                                        <div className="h-[100%] w-[25%] flex items-center justify-center">
+                                            <div className="h-17 w-17 bg-gray-400 flex items-center justify-center rounded-full">
+                                                I
+                                            </div>
+                                        </div>
+                                        <div className="bg-gray-00 h-[100%] w-[75%] flex flex-col justify-center">
+                                            <h3 className="font-semibold text-lg text-[#111827]">Jasmine</h3>
+                                            <h5 className="text-md text-[#575757] font-medium">Leave Request</h5>
+                                            <p className="text-xs text-[#575757] mt-1">Family Event</p>
+                                        </div>
+                                    </div>
+                                    <div className="h-[100%] w-[45%] rounded-r-lg p-2 flex items-center justify-center gap-2">
+                                        <button className="bg-[#3DB482] p-2 rounded-lg w-30 text-[#F9FAFB] font-semibold cursor-pointer">Approve</button>
+                                        <button className="bg-[#F66557] text-[#F9FAFB] font-semibold p-2 rounded-lg w-30 cursor-pointer">Reject</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-[#F9FAFB] h-50 shadow-md mt-2 rounded-lg p-5 pt-4">
+                            <div className="bg-green-00 h-[60%] flex flex-col">
+                                <h2 className="text-[#111827] font-medium">Financial Raise</h2>
+                                <div className="bg-yellow-00 h-[60%] flex items-center gap-5">
+                                    <div className="flex flex-col items-center justify-center gap-1 bg-indigo-00">
+                                        <CheckCircle size={39} color="#51B677" weight="fill" />
+                                        <h3 className="text-[#6C6C6C] font-medium text-xs">Approval</h3>
+                                    </div>
+                                    <div className="flex flex-col items-center justify-center bg-red-00 h-15 gap-1.5">
+                                            <WarningCircle size={39} color="#FABD48" weight="fill"/>
+                                        <h3 className="text-[#6C6C6C] font-medium text-xs">Pending</h3>
+                                    </div>
+                                    <div className="flex flex-col items-center justify-center bg-red-00 h-15 gap-1.5">
+                                            <XCircle size={39} color="#DD6468" weight="fill"/>
+                                        <h3 className="text-[#6C6C6C] font-medium text-xs">Rejected</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="h-[40%] flex flex-col justify-center gap-1">
+                                <h3 className="text-[#111827] font-medium text-md">FAQ</h3>
+                                <input
+                                    type="text"
+                                    placeholder="Raise a concern"
+                                    className="border border-1 border-[#B3B3B3] text-[#6C6C6C] w-[100%] h-[60%] rounded-lg px-2 focus:outline-none text-xs"
+                                />
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
