c54244e8537f332634b3630eb5ff5f59e73e4bd1
gkelite-info <gkeliteinfo@gmail.com>
2025-09-19 16:53:06 +0530

    pushing

--- Features/admin/components/BackupDateRestore.tsx
+++ Features/admin/components/BackupDateRestore.tsx
@@ -1,15 +1,21 @@
 "use client"
+import { label } from "framer-motion/client"
+import { useRouter } from "next/navigation"
+import path from "path"
 import { CaretRight } from "phosphor-react"
 import React from "react"
 
 const backupItems = [
-  "Recent Backup Status",
-  "Archival Schedule",
-  "Retention Alert",
-  "Data Backup Progress",
+  { label: 'Recent Backup Status', path: '/admin/backup&restoration' },
+  { label: 'Archival Schedule', path: '/admin/backup&restoration' },
+  { label: 'Retention Alert', path: '/admin/backup&restoration' },
+  { label: 'Data Backup Progress', path: '/admin/backup&restoration' },
 ]
 
 const BackupDateRestore: React.FC = () => {
+
+  const router = useRouter();
+
   return (
     <div className="w-full lg:w-1/2 bg-white rounded-lg p-4 flex flex-col gap-4 shadow">
       <h2 className="text-[#111827] font-semibold text-sm">
@@ -18,11 +24,12 @@ const BackupDateRestore: React.FC = () => {
 
       {backupItems.map((item) => (
         <div
-          key={item}
-          className="w-full flex justify-between items-center gap-2 cursor-pointer hover:bg-gray-50 rounded-md"
+          key={item.label}
+          onClick={() => router.push(item.path)}
+          className="w-full flex justify-between items-center gap-2 cursor-pointer rounded-md"
         >
-          <span className="text-xs text-[#464646]">{item}</span>
-          <CaretRight size={15} />
+          <span className="text-xs text-[#464646]">{item.label}</span>
+          <CaretRight size={15} className="text-[#111827]" />
         </div>
       ))}
     </div>
--- Features/admin/components/FullPieGraph.tsx
+++ Features/admin/components/FullPieGraph.tsx
@@ -2,53 +2,55 @@
 import React from "react"
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from "recharts"
 
-const FullPieGrapgh = ({ data, title, variant = "full" }) => {
+const FullPieGrapgh = ({ data, title, variant = "full", style = 'flex-col', linkStyle = 'flex', shadow = 'shadow-none' }: { data?: any, title?: string, variant?: string, style?: string, linkStyle?: string, shadow: string }) => {
   return (
-    <div className=" bg-white rounded-lg p-3 flex flex-col items-center justify-center gap-1.5">
+    <div className={`bg-white rounded-lg p-3 flex flex-col items-center justify-center gap-1.5 ${shadow}`}>
       <h3 className="text-sm  text-gray-700 mb-2">{title || "Pie Graph"}</h3>
-
-      <div className="w-35 h-30 relative flex items-center justify-center">
-        <ResponsiveContainer width="100%" height="100%">
-          <PieChart>
-            <Pie
-              data={data}
-              dataKey="value"
-              nameKey="legend"
-              innerRadius={variant === "donut" ? 30 : 0} // full = 0, donut = 40
-              outerRadius={60}
-              startAngle={90}
-              endAngle={-270}
-              paddingAngle={1}
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={entry.color || `hsl(${(index * 60) % 360} 70% 50%)`}
-                  stroke="none"
+      <div className={`${style} flex items-center gap-3`}>
+        <div className="w-35 h-30 relative flex items-center justify-center">
+          <ResponsiveContainer width="100%" height="100%">
+            <PieChart>
+              <Pie
+                data={data}
+                dataKey="value"
+                nameKey="legend"
+                innerRadius={variant === "donut" ? 30 : 0}
+                outerRadius={60}
+                startAngle={90}
+                endAngle={-270}
+                paddingAngle={1}
+              >
+                {data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={entry.color || `hsl(${(index * 60) % 360} 70% 50%)`}
+                    stroke="none"
+                  />
+                ))}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="flex-1">
+          <ul className={`gap-2 ${linkStyle}`}>
+            {data.map((d, i) => (
+              <li key={`legend-${i}`} className="flex items-center gap-2">
+                <span
+                  className="w-2 h-2 rounded"
+                  style={{
+                    background: d.color || `hsl(${(i * 60) % 360} 70% 50%)`,
+                  }}
                 />
-              ))}
-            </Pie>
-          </PieChart>
-        </ResponsiveContainer>
-      </div>
-      <div className="flex-1">
-        <ul className="flex  gap-2">
-          {data.map((d, i) => (
-            <li key={`legend-${i}`} className="flex items-center gap-2">
-              <span
-                className="w-2 h-2 rounded"
-                style={{
-                  background: d.color || `hsl(${(i * 60) % 360} 70% 50%)`,
-                }}
-              />
 
-              <span className="font-medium text-[10px] text-gray-800">
-                {d.legend}
-              </span>
-            </li>
-          ))}
-        </ul>
+                <span className="font-medium text-[10px] text-gray-800">
+                  {d.legend}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
+
     </div>
   )
 }
